7466ed05af8d1b7fbcb47ac1173ab6fb656bee66
alialtaaf009-glitch <alialtaaf009@gmail.com>
2026-06-11 15:47:42 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/Auth.jsx';
 import { ThemeProvider } from './context/Theme.jsx';
+import { TimerProvider } from './context/Timer.jsx';
 import './styles/app.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <TimerProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </TimerProvider>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
+
