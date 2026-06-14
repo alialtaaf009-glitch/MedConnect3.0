e00995856e5f70cabd7abecb9c9b9ea9f0f526b0
alialtaaf009-glitch <alialtaaf009@gmail.com>
2026-06-14 20:06:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import Motivation from './pages/Motivation.jsx';
 import Legal from './pages/Legal.jsx';
 import Reset from './pages/Reset.jsx';
 import About from './pages/About.jsx';
+import LabValues from './pages/LabValues.jsx';
 import Checklist from './components/Checklist.jsx';
 import AddPartner from './pages/AddPartner.jsx';
 
@@ -122,6 +123,11 @@ function Drawer({ open, onClose, user }) {
           <div className="drawer-sect">Grow</div>
           <button className="drawer-item" onClick={inviteFriend}>
             <svg viewBox="0 0 24 24"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" /><circle cx="9" cy="7" r="4" /><path d="M22 21v-2a4 4 0 0 0-3-3.87" /><path d="M19 8v6M22 11h-6" /></svg>Invite a colleague
+          </button>
+          <div className="drawer-div" />
+          <div className="drawer-sect">Tools</div>
+          <button className="drawer-item" onClick={() => go('/labs')}>
+            <svg viewBox="0 0 24 24"><path d="M9 3h6M10 3v6.5L5.5 17a2 2 0 0 0 1.7 3h9.6a2 2 0 0 0 1.7-3L14 9.5V3" /><path d="M8 14h8" /></svg>Lab values
           </button>
           <div className="drawer-div" />
           <div className="drawer-sect">App</div>
@@ -292,6 +298,7 @@ export default function App() {
         <Route path="/motivation" element={<Motivation />} />
         <Route path="/legal" element={<Legal />} />
         <Route path="/about" element={<About />} />
+        <Route path="/labs" element={<LabValues />} />
         <Route path="/connections" element={<Connections />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/add/:id" element={<AddPartner />} />
