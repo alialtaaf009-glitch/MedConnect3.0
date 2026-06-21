c9271fb3f9d16890d13ff4c5116b2b45c3385960
alialtaaf009-glitch <alialtaaf009@gmail.com>
2026-06-21 18:08:47 +0500

    Update examColors.js

--- src/lib/examColors.js
+++ src/lib/examColors.js
@@ -1,12 +1,12 @@
 // shared per-exam signature colors (v1 vibrancy system)
 export const EXAM_COLORS = {
-  'USMLE': '#1a5a8a', 'MRCP': '#1a6b5a', 'MRCS': '#2a6a8a', 'MRCPCH': '#1a7a4a',
-  'MRCGP': '#3a7a4a', 'MRCPath': '#2a5a6a', 'MRCEM': '#1a5f7a', 'MRCOG': '#2e4a7a',
-  'MRCPsych': '#1a6b8a', 'PLAB / UKMLA': '#1a7a6a', 'PLAB': '#1a7a6a', 'FCPS Part 1': '#1a7a4a',
-  'FCPS Part 2': '#3a6a3a', 'FCPS': '#1a7a4a', 'IMM': '#2a6a7a', 'MCPS': '#3a7a4a',
-  'AMC': '#1a5f7a', 'RACP': '#1a6b5a', 'RACS': '#2a5a6a',
+  'USMLE': '#1a5a8a', 'MRCP': '#1f5d4a', 'MRCS': '#2a6a8a', 'MRCPCH': '#1f6048',
+  'MRCGP': '#2f6a44', 'MRCPath': '#2a5a6a', 'MRCEM': '#1a5f7a', 'MRCOG': '#2e4a7a',
+  'MRCPsych': '#1a6b8a', 'PLAB / UKMLA': '#1f6354', 'PLAB': '#1f6354', 'FCPS Part 1': '#1f6048',
+  'FCPS Part 2': '#306034', 'FCPS': '#1f6048', 'IMM': '#2a6a7a', 'MCPS': '#2f6a44',
+  'AMC': '#1a5f7a', 'RACP': '#1f5d4a', 'RACS': '#2a5a6a',
   'SMLE': '#2e4a7a', 'Saudi Board': '#1a6b8a', 'SCFHS Prometric': '#2a6a8a',
-  'NEET-PG': '#3a7a4a', 'INI-CET': '#1a7a6a', 'FMGE / NExT': '#1a5a8a', 'NEET-SS': '#2a6a7a',
+  'NEET-PG': '#2f6a44', 'INI-CET': '#1f6354', 'FMGE / NExT': '#1a5a8a', 'NEET-SS': '#2a6a7a',
 };
 export const examColor = (exam) => {
   if (!exam) return 'var(--forest)';
